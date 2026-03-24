5203bc0fd19d660754b2dc462bee07409ad751b5
brian-gustavo <brian20062005@gmail.com>
2026-03-24 01:55:16 -0300

    Update

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)} # Gera a URL (slug) automaticamente quando o nome é digitado
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'seller', 'price', 'condition', 'created_at')
+    list_filter = ('category', 'condition')
+    search_fields = ('title', 'description')
+    inlines = [ProductImageInline]
